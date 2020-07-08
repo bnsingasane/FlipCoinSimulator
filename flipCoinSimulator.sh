a6f035722c79aef928819c7602001f44c0be3274
bhagyashree98 <bnsingasane@gmail.com>
2020-07-08 15:19:01 +0530

    ensure tie and find difference of min 2 pts achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,28 +1,45 @@
-#usecase3
 
-heads=0
-tails=0
 
-while [ $heads -le 21 ] && [ $tails -le 21 ]
+head=0
+tail=0
+while [ $head -lt 21 ] && [ $tail -lt 21 ]
 do
-	i=0
-	$i=$((i+1))
-	for((i=0;i<100;i++))
+        r=$((RANDOM % 2))
+        if [ $r -eq 1 ]
+        then
+                head=$(($head + 1))
+        else
+                tail=$(($tail + 1))
+        fi
+done
+
+if [ $head -eq $tail ]
+then
+	while true
 	do
-		r=$((RANDOM % 2)) 
-		if [ $r -eq 1 ]
+		rs=$((RANDOM % 2))
+		if [ $rs -eq 1 ]
 		then
-			heads=$(($heads+1))
-		else 
-			tails=$(($tails+1))
+			head=$(($head + 1))
+		else
+			tail=$(($tail + 1))
+		fi
+
+		if [ $(($head - $tail)) -eq 2 ]
+		then
+			echo "Head win by 2 and count is $head"
+		else
+			if [ $(($tail - $head)) -eq 2 ]
+			then
+				echo "Tail win by 2 and count is $tail"
+			fi
 		fi
 	done
- 
-if [ $heads -gt $tails ]
-then
-	echo "heads won $heads"
-else
-	echo "tails wins $tails"
+elif [ $head -gt $tail ]
+	then
+       		win=$(($head - $tail))
+		echo "Tail wins by $win"
+	else
+       		win=$(($tail - $head))
+       		echo "Tail wins by $win"
 fi
-done
-echo "number of rounds $i"
