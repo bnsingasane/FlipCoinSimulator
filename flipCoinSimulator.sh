ad08a849b039cc5b2d4996c36fe35aeb84026c52
bhagyashree98 <bnsingasane@gmail.com>
2020-07-08 15:05:52 +0530

    show if its win or tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,35 +1,23 @@
-#!/bin/bash -x
-#usecase1
-r=$((RANDOM % 2))
-echo "enter 1 for head and 0 for tails"
-read choice
- 
-if [ $r -eq 1 ]
-then 
-	 echo heads
-else 
-	echo tails
-fi
 
-if [ $r -eq $choice ]
-then 
-	echo "user won"
-else
-	echo "user lost"
-fi
-#usecase2
+#usecase3
 
-read times
-for((i=0;i<$times;i++))
+heads=0
+tails=0
+
+while [ $heads -le 21 ] && [ $tails -le 21 ]
 do
-	r=$((RANDOM % 2)) 
-	if [ $r -eq 1 ]
-	then
-		heads=$(($heads+1))
-	else 
-		tails=$(($tails+1))
-	fi
-done
+	i=0
+	$i=$((i+1))
+	for((i=0;i<100;i++))
+	do
+		r=$((RANDOM % 2)) 
+		if [ $r -eq 1 ]
+		then
+			heads=$(($heads+1))
+		else 
+			tails=$(($tails+1))
+		fi
+	done
  
 if [ $heads -gt $tails ]
 then
@@ -37,3 +25,5 @@ then
 else
 	echo "tails wins $tails"
 fi
+done
+echo "number of rounds $i"
