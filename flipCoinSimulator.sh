ff063d164fdec1aecee3b888bf68c3aa090b059b
bhagyashree98 <bnsingasane@gmail.com>
2020-07-08 14:55:32 +0530

    show no of times heads and tails won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -x
+#usecase1
+r=$((RANDOM % 2))
+echo "enter 1 for head and 0 for tails"
+read choice
+ 
+if [ $r -eq 1 ]
+then 
+	 echo heads
+else 
+	echo tails
+fi
 
+if [ $r -eq $choice ]
+then 
+	echo "user won"
+else
+	echo "user lost"
+fi
 #usecase2
 
 read times
